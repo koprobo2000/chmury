70b375c3503ded427ca38cc4c934bdaece89085c
“Beata <pawulonian@gmail.com>
2024-01-13 15:59:16 +0100

    people detector added

---
+++ main.py
@@ -0,0 +1,20 @@
+import cv2
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+image = cv2.imread('ludzie.jpg')
+image = cv2.resize(image, (500, 400))
+
+# detect people in the image
+(rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+
+# draw the bounding boxes
+for (x, y, w, h) in rects:
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+print(f'Found {len(rects)} humans')
+
+# show the output images
+cv2.imshow("People detector", image)
+cv2.waitKey(0)
