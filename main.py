48d378264f2782ef419f855510b2899f551c96c2
Student <student@it.uekat.local>
2024-01-14 13:44:28 +0100

    main

--- main.py
+++ main.py
@@ -14,9 +14,13 @@ api = Api(app)
 class PeopleCounter(Resource):
     def get(self):
 
+
+
+
         cv2.waitKey(0)
         image = cv2.imread('ludzie.jpg')
         image = cv2.resize(image, (1000, 900))
+
 # detect people in the image
         (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
         return {'peopleCount': len(rects)}
